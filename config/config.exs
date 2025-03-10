0a717a96ecc8dbc986e561d73649b96c6ec2ce5c
edmondfrank <edmondfrank@hotmail.com>
2025-03-10 22:28:56 +0800

    Add multiple LLM configurations and update proxy settings template.
    Signed-off-by: edmondfrank <edmondfrank@hotmail.com>

--- config/config.exs
+++ config/config.exs
@@ -2,8 +2,35 @@ import Config
 
 
 config :exhub,
-  llm: %{
-    api_base: "https://api.mistral.ai/v1",
-    api_key: "api_key",
-    model: "mistral/codestral-latest"
+  llms: %{
+    "qwen2.5-72b-instruct" => %{
+      api_base: "https://ai.gitee.com/v1",
+      api_key: "your token",
+      model: "openai/Qwen2.5-72B-Instruct",
+    },
+    "qwen2.5-32b-instruct" => %{
+      api_base: "https://api.siliconflow.cn/v1",
+      api_key: "your token",
+      model: "openai/Qwen/Qwen2.5-32B-Instruct",
+    },
+    "codestral-latest" => %{
+      api_base: "https://codestral.mistral.ai/v1",
+      api_key: "your token",
+      model: "mistral/codestral-latest",
+    },
+    "mistral-large-latest" => %{
+      api_base: "https://api.mistral.ai/v1",
+      api_key: "your token",
+      model: "mistral/mistral-large-latest",
+    },
+    "llama-3.3-70b-versatile" => %{
+      api_base: "http://127.0.0.1:9069/groq/v1",
+      api_key: "your token",
+      model: "openai/llama-3.3-70b-versatile",
+    }
+  },
+  proxy: "http://127.0.0.1:7890",
+  gitee_cat: %{
+    endpoint: "https://api.gitee.com/",
+    auth: %{cookie: "your cookies"}
   }
