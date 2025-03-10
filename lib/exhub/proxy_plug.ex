db5fddc568950da19eb52bcea467aa096a61550b
edmondfrank <edmondfrank@hotmail.com>
2025-03-10 22:24:22 +0800

    Add reverse proxy functionality to Exhub.
    Signed-off-by: edmondfrank <edmondfrank@hotmail.com>

---
+++ lib/exhub/proxy_plug.ex
@@ -0,0 +1,37 @@
+defmodule Exhub.ProxyPlug do
+
+  @doc """
+  reverse conn proxy to upstream
+  """
+  def forward_upstream(conn, upstream, opts \\ []) do
+    params =
+      ReverseProxyPlug.init(
+        upstream: upstream,
+        response_mode: Keyword.get(opts, :response_mode, :buffer),
+        client_options: Keyword.get(opts, :client_options, []),
+        preserve_host_header: Keyword.get(opts, :preserve_host_header, false)
+      )
+
+    {pre_body, conn} = ReverseProxyPlug.read_body(conn)
+
+    body =
+      cond do
+        pre_body == "" && Plug.Conn.get_req_header(conn, "content-type") == ["application/json"] ->
+          Jason.encode!(conn.body_params)
+
+        pre_body == "" &&
+            Plug.Conn.get_req_header(conn, "content-type") == [
+              "application/x-www-form-urlencoded"
+            ] ->
+          Plug.Conn.Query.encode(conn.body_params)
+
+        true ->
+          pre_body
+    end
+
+    conn
+    |> Map.put(:path_info, conn.path_params["path"])
+    |> ReverseProxyPlug.request(body, params)
+    |> ReverseProxyPlug.response(conn, params)
+  end
+end
--- lib/exhub/router.ex
+++ lib/exhub/router.ex
@@ -1,17 +1,24 @@
 defmodule Exhub.Router do
+  alias Exhub.ProxyPlug
+  @proxy Application.compile_env(:exhub, :proxy, "")
   require Logger
   use Plug.Router
   use PlugSocket
 
   plug Plug.Parsers,
-        parsers: [:json],
-        pass:  ["application/json"],
-        json_decoder: Jason
+        parsers: [:urlencoded, {:json, json_decoder: Jason}],
+        pass:  ["*/*"]
   socket "/exhub", Exhub.SocketHandler
   plug :match
   plug :dispatch
 
-  get _ do
+
+
+  post "/groq/v1/*path" do
+    ProxyPlug.forward_upstream(conn, "https://api.groq.com/openai/v1", client_options: [proxy: @proxy])
+  end
+
+  match _ do
     send_resp(conn, 200, "<html><head></head><body>Connect to WS enpoint at \"/exhub\"</body></html>")
   end
 end
